f11f1c880d39beaff53e4ef96d1dff24684ce740
Wayne <wayne80361@gmail.com>
2023-08-24 18:26:33 -0400

    function ask password length and lowercase.

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  uselowerCase();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,18 +13,33 @@ function writePassword() {
 function generatePassword() {
   // do stuff
   // ask how long the password should be?
-  // check if the password is betweem 8- 128 characters
+  var passwordLength = prompt(
+    "Please choose the length or your desire password between 8 - 128 characters"
+    // check if the password is betweem 8- 128 characters
+  );
+  if (128 >= passwordLength && passwordLength >= 8) {
+    return passwordLength;
+  }
   // --no? ask again
-  // are we using lowercase characters?
-  // --yes? put the lowercase characters into our list of chosen characters
-  // are we using uppercase characters?
-  // are we using special characters?
-  // are we using numerical characters?
-  // based on the chosen length select random characters from the chosen characters set and put them in the password
-
-  var password = "password";
-  return password;
+  return generatePassword();
 }
+
+function uselowerCase() {
+  // are we using lowercase characters?
+  if (confirm("Would you like to use lowercase in the password?")) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+// --yes? put the lowercase characters into our list of chosen characters
+// are we using uppercase characters?
+// are we using special characters?
+// are we using numerical characters?
+// based on the chosen length select random characters from the chosen characters set and put them in the password
+
+// var password = "password";
+// return password;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
