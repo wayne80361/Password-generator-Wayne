798c7567000f9cd2fab9d02c794d2e941f89ab41
Wayne <wayne80361@gmail.com>
2023-08-24 21:54:12 -0400

    re-code the function of asking lower, upper, special, numerical characters so the result would log to the 'function writePassword()'.

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,13 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  uselowerCase();
-  useUpperCase();
-  useSpecialChar();
-  useNumChar();
+  var picklowerCase = uselowerCase();
+  var pickupperCase = useUpperCase();
+  var pickspecialChar = useSpecialChar();
+  var picknumChar = useNumChar();
+
+  var userspicks = [picklowerCase, pickupperCase, pickspecialChar, picknumChar];
+  console.log(userspicks);
 
   var passwordText = document.querySelector("#password");
 
@@ -35,37 +38,61 @@ function generatePassword() {
 // are we using lowercase characters?
 function uselowerCase() {
   // --yes? put the lowercase characters into our list of chosen characters
-  var lowerCase = confirm("Would you like to use lowercase in the password?");
-
-  console.log(lowerCase);
+  var picklowerCase = confirm(
+    "Would you like to use lowercase in the password?"
+  );
+  if (picklowerCase == true) {
+    console.log(picklowerCase);
+    return picklowerCase;
+  } else {
+    console.log(picklowerCase);
+    return false;
+  }
 }
 
 // are we using uppercase characters?
 function useUpperCase() {
-  var upperCase = confirm(
+  var pickupperCase = confirm(
     "How about Uppercase? Would you like it to be in the password?"
   );
-  console.log(upperCase);
+  if (pickupperCase == true) {
+    console.log(pickupperCase);
+    return pickupperCase;
+  } else {
+    console.log(pickupperCase);
+    return false;
+  }
 }
 
 // are we using special characters?
 function useSpecialChar() {
-  var specialChar = confirm(
+  var pickspecialChar = confirm(
     "Specials!! They are great for security, would you like blend some?"
   );
-  console.log(specialChar);
+  if (pickspecialChar == true) {
+    console.log(pickspecialChar);
+    return pickspecialChar;
+  } else {
+    console.log(pickspecialChar);
+    return false;
+  }
 }
 
 // are we using numerical characters?
 function useNumChar() {
-  var numChar = confirm(
+  var picknumChar = confirm(
     "Numerical? Would you like to mix them into the password?"
   );
-  console.log(numChar);
+  if (picknumChar == true) {
+    console.log(picknumChar);
+    return picknumChar;
+  } else {
+    console.log(picknumChar);
+    return false;
+  }
 }
 
 // based on the chosen length select random characters from the chosen characters set and put them in the password
-
 // var password = "password";
 // return password;
 
